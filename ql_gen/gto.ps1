f2f73c8baca4dfe4af36f69e1de3a4cd043b2845
roadroot <50836748+roadroot@users.noreply.github.com>
2025-04-15 18:38:27 +0200

    refactor: import ql_gen as a published library (#73)

--- ql_gen/gto.ps1
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Use this to get the test case output
-
-$case = $args[0]
-
-Write-Output "generating $case"
-
-dart run bin/ql_gen.dart -s static/test/$case/input.gql -t static/test/$case/output.dart
--- ql_gen/test/src/generation/mutation/mutation_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-
-import '../generation_util.dart';
-
-void main() {
-  group('mutation tests', () {
-    test('mutation must be generated', () => GenUtils.test('mutation'));
-  });
-}
