e36be47d6273d17b63cd71c97151b3dbaccfd6cb
roadroot <mohamed.azikiou@gmail.com>
2025-04-15 18:33:04 +0200

    refactor: import ql_gen as a published library

--- ql_gen/lib/ql_gen.dart
+++
@@ -1,9 +0,0 @@
-/// Support for doing something awesome.
-///
-/// More dartdocs go here.
-library ql_gen;
-
-export 'src/ql_gen_base.dart';
-export 'src/ql_gen/ql_parser.dart';
-
-// TODO: Export any libraries intended for clients of this package.
--- ql_gen/test/src/generation/subscription/subscription_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:test/test.dart';
-
-import '../generation_util.dart';
-
-void main() {
-  group('subscription tests', () {
-    test('subscription must be generated', () => GenUtils.test('subscription'));
-  });
-}
